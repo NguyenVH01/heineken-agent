2dd6644eb706d99266869f8124696ab15d822a8a
NguyenVH01 <163318515+NguyenVH01@users.noreply.github.com>
2024-06-23 17:23:11 +0000

    change config

--- .venv/lib/python3.12/site-packages/llama_index/experimental/__init__.py
+++
@@ -1,6 +0,0 @@
-from llama_index.experimental.query_engine.pandas.pandas_query_engine import (
-    PandasQueryEngine,
-)
-from llama_index.experimental.param_tuner.base import ParamTuner
-
-__all__ = ["PandasQueryEngine", "ParamTuner"]
--- .venv/lib/python3.12/site-packages/pandasai/ee/vectorstores/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Vector stores to store data for training purpose
-"""
-
-from .chroma import ChromaDB
-from .qdrant import Qdrant
-
-__all__ = ["ChromaDB", "Qdrant"]
