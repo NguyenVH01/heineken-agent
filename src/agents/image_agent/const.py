d593362eafb1c1cc62ce64282b91c97220e571ba
NguyenVH01 <nguyenvuhoangwork@gmail.com>
2024-06-30 09:46:42 +0700

    add prompt

--- src/agents/image_agent/const.py
+++ src/agents/image_agent/const.py
@@ -3,7 +3,7 @@ from enum import Enum
 class ImageAnalysisAction(Enum):
   SOLUTION_ONE = "Detect human and drinkers"
   SOLUTION_TWO = "Detect print adversting"
-  SOLUTION_THREE = "Evaluate success event"
+  SOLUTION_THREE = "Assess success event"
   SOLUTION_FOUR = "Follow marketing staff"
   SOLUTION_FIVE = "Assess retail presence"
   ANALYZE_NEW = "Analyze new image"
